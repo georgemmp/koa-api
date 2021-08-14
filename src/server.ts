873c3b16be3b9789cda5bba79fea90a346da3d8d
georgemmp <machadogeorge.p@gmail.com>
2021-08-14 19:36:02 -0300

    Configuração de projeto e instalação do koa

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import '@config/environments.config';
+
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.listen(process.env.PORT);
+
+console.log(`Server listening on PORT ${process.env.PORT}`)
