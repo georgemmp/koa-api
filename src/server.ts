56510a6cff538626b15bf9a9f408b8deed6cc2c8
georgemmp <machadogeorge.p@gmail.com>
2021-08-15 14:20:14 -0300

    Koa body-parser

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import '@config/environments.config';
 
 import { logger } from '@shared/logger';
+
 import router from './router';
 import koaLogger from 'koa-logger';
+import bodyPaser from 'koa-parser';
 
 import Koa from 'koa';
 
 const app = new Koa();
 
+app.use(bodyPaser());
 app.use(koaLogger());
-
 app.use(router.routes());
 
 app.listen(process.env.PORT);
 
-logger.info(`Server listening on PORT ${process.env.PORT}`)
+logger.info(`Server listening on PORT ${process.env.PORT}`);
