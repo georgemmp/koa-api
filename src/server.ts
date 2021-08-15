c131f11166fe8b4afa7bc1eb3f066f9d0e93a275
georgemmp <machadogeorge.p@gmail.com>
2021-08-15 10:26:53 -0300

    Hello World iniciando o koa

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ import Koa from 'koa';
 
 const app = new Koa();
 
+app.use(async ctx => {
+  ctx.body = "Hello World"!;
+})
+
 app.listen(process.env.PORT);
 
 console.log(`Server listening on PORT ${process.env.PORT}`)
